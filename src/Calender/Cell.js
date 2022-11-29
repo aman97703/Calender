1ee4f36b76f1d091c104d9b4df0de01311d3ad94
Aman Gupta <aman97703@gmail.com>
2022-11-30 00:47:53 +0530

    update

--- src/Calender/Cell.js
+++ src/Calender/Cell.js
@@ -6,7 +6,7 @@ const Cell = ({children, className, onClick, isActive, eventDate, edate}) => {
     if(eventDate && eventDate.length > 0){
       setEventExist(eventDate.includes(edate))
     }
-  },[eventDate])
+  },[eventDate, edate])
   return (
     <div onClick={onClick} className={className+` h-12 flex items-center justify-center border-r border-b ${!isActive && onClick && "hover:bg-gray-100 cursor-pointer active:bg-gray-200" } ${isActive && 'bg-blue-600 text-white'} ${eventExist ? 'bg-green-600 text-white hover:bg-green-500':''}`}>{children}</div>
   )
